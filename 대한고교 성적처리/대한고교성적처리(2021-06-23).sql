7779d02a053fa31ef6b52b0ff03663e1984f8d20
wndeld777 <wndeld777@gmail.com>
2021-07-12 17:33:28 +0900

    honjal

--- 대한고교 성적처리/대한고교성적처리(2021-06-23).sql
+++ 대한고교 성적처리/대한고교성적처리(2021-06-23).sql
@@ -14,13 +14,40 @@ INSERT INTO tbl_student(st_num,st_name,st_dept,st_grade,st_tel)
 values('2021001','홍길동','학과','1','010-111-222');
 
 commit;
+
+/*
+TABLE에 
+	INSERT INTO ON DUPLICATE KEY UPDATE를
+    실행하기 위해서는 PK 설정을 변경해야 한다
+tbl_score은 두개의 칼럼을 기준으로
+		 UPDATE, DELETE 를 수행하는 문제가 발생한다
+가장 좋은 설계는 UPDATE, DELETE를 수행할때
+		한개의 칼럼으로 구성된 PK를 기준으로
+        수행하는 것이다
+*/
+CREATE TABLE tbl_score(
+	-- sc_seq BIGINT auto_increment PRIMARY KEY,
+    sc_stnum char(8) NOT NULL,
+    sc_sbcode char(4) NOT NULL,
+    sc_score INT NOT NULL,
+    PRIMARY KEY(sc_stnum, sc_sbcode)
+);
+/*
+PK는 그대로 살려두고 
+두개의 칼럼을 묶어 UNIQUE로 설정 
+두개 칼럼의 값이 동시에 같은 경우는 추가하지 말라는
+ 제약조건 설정
+*/
 CREATE TABLE tbl_score(
 	sc_seq BIGINT auto_increment PRIMARY KEY,
     sc_stnum char(8) NOT NULL,
     sc_sbcode char(4) NOT NULL,
-    sc_score INT NOT NULL
+    sc_score INT NOT NULL,
+    unique(sc_stnum, sc_sbcode)
 );
 DROP TABLE tbl_score;
+SELECT * FROM tbl_score;
+SELECT * FROM tbl_subject;
 DESC tbl_score;
 INSERT INTO tbl_score(sc_stnum,sc_sbcode,sc_score)
 values('2021001','S001',90);
@@ -46,3 +73,22 @@ INSERT INTO tbl_subject(sb_code,sb_name)
 value('S002','영어');
 INSERT INTO tbl_subject(sb_code,sb_name)
 value('S003','수학');
+
+-- 한학생의 세과목의 점수를 개별적으로 INSERT 하기
+INSERT INTO tbl_score(sc_stnum, sc_sbcode,sc_score)
+values('20210002','S001',90);
+INSERT INTO tbl_score(sc_stnum, sc_sbcode,sc_score)
+values('20210002','S002',90);
+INSERT INTO tbl_score(sc_stnum, sc_sbcode,sc_score)
+values('20210002','S003',90);
+
+-- 한번의 INSERT 명령문으로 다수의 데이터를 INSERT
+-- BULK INSERT
+INSERT INTO tbl_score(sc_stnum, sc_sbcode, sc_score)
+values('2021002','S001',90),
+('2021002','S002',90),
+('2021002','S003',90),
+('2021002','S004',90),
+('2021002','S005',90);
+
+select * from tbl_score;
---
+++ 이미지갤러리/이미지갤러리(2021-07-06).sql
@@ -0,0 +1,93 @@
+USE mylibs;
+
+CREATE TABLE tbl_gallery(
+	g_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	g_writer	VARCHAR(20)	NOT NULL	,
+	g_date	VARCHAR(10)	NOT NULL	,
+	g_time	VARCHAR(10)	NOT NULL	,
+	g_subject	VARCHAR(50)	NOT NULL,	
+	g_content	VARCHAR(1000)	NOT NULL	,
+	g_image	VARCHAR(125)		
+);
+SHOW TABLES;
+SELECT * FROM tbl_gallery;
+SELECT COUNT(*) FROM tbl_gallery;
+DROP TABLE tbl_gallery;
+
+CREATE TABLE tbl_files(
+	file_seq	BIGINT	AUTO_INCREMENT	PRIMARY KEY,
+	file_gseq	BIGINT	NOT NULL	,
+	file_original	VARCHAR(125)	NOT NULL	,
+	file_upname	VARCHAR(125)	NOT NULL	
+);
+DROP TABLE tbl_files;
+
+INSERT INTO tbl_gallery
+(g_writer,g_date,g_time,g_subject,g_content)
+VALUES
+('wndeld777','2021-07-06','15:18:00',' 연습','진짜연습');
+
+-- 현재 연결된 session에서 INSERT가 수행되고 
+-- 그 과정에서 AUTO_INCREMENT 칼럼이 변화가 있으면
+-- 그 값을 알려주는 함수 
+SELECT LAST_INSERT_ID();
+
+SELECT * FROM tbl_member;
+DROP TABLE tbl_member;
+
+/*
+ view_갤러리의 SQL 코드 
+ EQ JOIN을 만들어서 보여지는 코드
+ tbl_gallery에는 데이터가 있는데 
+ tbl_files에 연결되는 데이터가 하나도 없는경우
+ tbl_gallery 자체가 출력되지 않는 문제가 있다
+*/
+SELECT G.g_seq AS g_seq,
+		 G.g_writer AS g_writer, 
+		 G.g_date AS g_date, 
+		 G.g_time AS g_time, 
+		 G.g_subject AS g_subject, 
+		 G.g_content AS g_content, 
+		 G.g_image AS g_image,
+		 
+		F.file_seq AS f_seq, 
+		F.file_original AS f_original, 
+		F.file_upname AS f_upname
+	FROM tbl_gallery G,tbl_files F
+			WHERE G.g_seq = F.file_gseq
+				AND g.g_seq = 6;
+/*
+	1:N 관계의 table일 경우
+    보통은 FK로 설정하여 데이터를 유지한다
+    
+    1:0..N : child table에 연관된 데이터가 하나도 없는 경우가 있다 
+    1:1..N : child table에 연관된 데이터가 최소 한개는 있는 경우 
+    
+    1:1..N 인경우는 EQ JOIN을 수행해도 실제로 Parent table에 있는 데이터는 무조건 출력이 된다
+    1:0..N 인경우는 child table에 데이터가 하나도 없는 경우 EQ JOIN을 수행하면 출력되는 데이터가 한개도 없는 상황이 발생한다
+    
+    JOIN을 수행할때는 FK가 설정되는 경우가 잇거나 말거나
+		JOIN은 LEFT(OUTTER) JOIN을 수행하는 것이 좋다
+*/
+SELECT G.g_seq AS g_seq,
+		 G.g_writer AS g_writer, 
+		 G.g_date AS g_date, 
+		 G.g_time AS g_time, 
+		 G.g_subject AS g_subject, 
+		 G.g_content AS g_content, 
+		 G.g_image AS g_image,
+		 
+		F.file_seq AS f_seq, 
+		F.file_original AS f_original, 
+		F.file_upname AS f_upname
+	FROM tbl_gallery G
+		LEFT JOIN tbl_files F
+			ON G.g_seq = F.file_gseq
+				AND g.g_seq = 6;
+            
+DELETE from tbl_files
+WHERE file_seq = 15;
+
+SELECT max(g_seq) FROM tbl_gallery;
+UPDATE tbl_gallery SET g_image = null
+WHERE g_seq = 6;
